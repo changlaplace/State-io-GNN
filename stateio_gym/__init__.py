309759e5eba3492fc3f64d87d46b1812038e3202
Yub <changlaplace@gmail.com>
2025-05-27 07:08:30 +0000

    Runnable frame

--- stateio_gym/__init__.py
+++ stateio_gym/__init__.py
@@ -2,5 +2,5 @@ from gymnasium.envs.registration import register
 
 register(
     id="StateIO-v0",  
-    entry_point="stateio_gym:StateIOEnv", 
+    entry_point="stateio_gym.stateio_env:StateIOEnv", 
 )
